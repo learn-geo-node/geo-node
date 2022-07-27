7201375e6aab6a33d0eb38aab2d1a006dd6b8e39
mihelox21 <michal.nosal21@gmail.com>
2022-07-27 09:47:06 +0200

    add(): cleaned up env's and add Makefile

--- src/app-config.ts
+++ src/app-config.ts
@@ -1,8 +1,3 @@
-// TODO: investigate that we can all use env's without dotenv
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
 export const configuration = {
   port: process.env.PORT,
 } as const;
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,3 @@
-require('dotenv').config()
 import 'reflect-metadata';
 import { AppDataSource } from './config';
 
